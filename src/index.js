2a10468247f9d10e2201d2764db3df50524f015a
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-07 15:21:10 +0545

    Using subscribe and unsubscribe properties

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import store from "./store";
 
+const unsubscribe = store.subscribe(() => {
+  console.log("Store Change!", store.getState());
+});
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "Bug1",
+  },
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
   },
 });
 console.log(store.getState());
