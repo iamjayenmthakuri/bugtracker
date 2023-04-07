e7a0cc819236cd8a24c3e71ee8b331756b81fdcc
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-07 15:13:51 +0545

    dispaching first bug as "Bug1"

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,18 @@
+/* eslint-disable eqeqeq */
 let lastId = 0;
-export default function reducer(state = [],action) {
-    switch(action.type) {
-        case "bugAdded":
-           return [
-             ...state,
-             {
-                id: ++lastId
-                description: action.payload.description,
-                resolved: false
-              }
-            ];
-        case "bugRemoved":
-            return state.filter(bug => bug.id != action.payload.id)
-
-        defaut:
-             return state;
-    }
+export default function reducer(state = [], action) {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case "bugRemoved":
+      return state.filter((bug) => bug.id != action.payload.id);
+  }
 }
